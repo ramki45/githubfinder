9aab7f1db62398ac9500a9e49933f7450919c4c8
Ramki Marichamy <ramkipandian@ramkis-MacBook-Pro.local>
2022-03-28 12:11:01 +0530

    first commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,20 @@
+function About() {
+    return (
+      <>
+        <h1 className='text-6xl mb-4'>Github Finder</h1>
+        <p className='mb-4 text-2xl font-light'>
+          A React app to search GitHub profiles and see profile details.
+        </p>
+        
+        <p className='text-lg text-gray-400'>
+          Layout By: 
+          
+        </p>
+        <p> <a className='text-green' href='https://twitter.com/marichamy_ramki'>
+            Ramki Marichamy
+          </a> </p>
+      </>
+    )
+  }
+  
+  export default About
